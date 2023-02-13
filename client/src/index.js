8731bf72f4eea031b56cd45fd6c911d2a749b871
Zetah07 <sebastiancastro0792@gmail.com>
2023-02-13 17:54:18 -0500

    structuring front

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,33 @@
-import React from 'react';
+import React, {Suspense} from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import {BrowserRouter} from 'react-router-dom';
+import {Provider} from 'react-redux';
+import store from './redux/store';
+import axios from 'axios';
 import reportWebVitals from './reportWebVitals';
+
+//firebase:
+import {FirebaseAppProvider} from 'reactfire';
+import {firebaseConfig} from './firebaseConfig';
+
+//axios:
+axios.defaults.baseURL = 'http://localhost:3001' && process.env.REACT_APP_API;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <FirebaseAppProvider firebaseConfig={firebaseConfig}>
+    <Suspense fallback={"Conecting ..."}>
+      <Provider store={store}>
+        <BrowserRouter>
+          <React.StrictMode>
+            <App />
+          </React.StrictMode>
+        </BrowserRouter>
+      </Provider>
+    </Suspense>
+  </FirebaseAppProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
