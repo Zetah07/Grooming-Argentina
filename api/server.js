e85220aa064ece90c947c6a7db58b31653ceaab5
multichiki22 <myguelangel12@gmail.com>
2023-02-14 15:04:19 -0500

    More dependencies And better order

---
+++ api/server.js
@@ -0,0 +1,16 @@
+const express = require("express")
+const app = express()
+const PORT = process.env.PORT || 3500
+const cors = require("cors")
+const morgan = require('morgan');
+const corsOptions = require("./config/corsOptions")
+
+//Cross Origin Resource Service
+app.use(cors(corsOptions));
+
+app.use(morgan("dev"));
+
+app.use('/', require("./routes/MainRouter"))
+
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
