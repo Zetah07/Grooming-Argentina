f6d911bfa2a0e9c7322fc1bfd1c52896ae7a7e3a
Zetah07 <sebastiancastro0792@gmail.com>
2023-02-13 19:31:50 -0500

    routes of APP

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/NavBar';
+import LandingPage from './components/LandingPage';
+import About from './components/Pages/About';
+import Home from './components/Home';
+import News from './components/News';
+import NewsDetail from './components/NewsDetail';
+import Volunteer from './components/Pages/Volunteer';
+import Login from './components/Pages/Login';
+
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-      
+      <NavBar />
       </header>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route exact path="/voluntariado" element ={<Volunteer />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="noticias" element={<News />}/>
+        <Route path="/noticias/:id" element={<NewsDetail />} />
+        <Route path="/nosotros" element={<About />} />
+      </Routes>
     </div>
   );
 }
