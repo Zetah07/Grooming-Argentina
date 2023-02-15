1b67ac0eab00029dd4a0892dc70a243e9f41861a
akanashimi <113636928+akanashimi@users.noreply.github.com>
2023-02-15 10:56:01 -0600

    Pagination, news and cards

--- client/src/components/Pages/Pagination/Pagination.jsx
+++ client/src/components/Pages/Pagination/Pagination.jsx
@@ -1,8 +1,36 @@
 import React from 'react'
+import style from "./Pagination.module.css";
 
-const Pagination = () => {
-  return (
-    <div>Pagination</div>
+const Pagination = ({ totalNews, firstHandler, prevHandler, nextHandler, lastHandler, pages, newsPerPage, currentPage, pageNumberLimit, maxPageNumberLimit, minPageNumberLimit }) => {
+
+  const numOfPages = [];
+  const firstPage = 0;
+  let lastPage = 0;
+  const amountOfPages = Math.ceil(totalNews / newsPerPage);
+  for (let i = 0; i < amountOfPages; i++) {
+    lastPage = i;
+    numOfPages.push(i);
+  }
+
+  return (<>
+    <div>
+      <div className={style.buttons}>
+        <button onClick={() => firstHandler(firstPage)} className={currentPage === firstPage ? style.prevButtonActive : style.prevButton}>First</button>
+        <button onClick={prevHandler} className={currentPage === firstPage ? style.prevButtonActive : style.prevButton}>Prev</button>
+        {
+          numOfPages?.map((page) => {
+            if (page < maxPageNumberLimit && page >= minPageNumberLimit) {
+              return <button className={page === currentPage ? style.pageButtonActive : style.pageButton} id={page} key={page} onClick={() => pages(page)}>{page + 1}</button>
+            } else {
+              return null;
+            }
+          })
+        }
+        <button onClick={nextHandler} className={currentPage === lastPage ? style.nextButtonActive : style.nextButton}>Next</button>
+        <button onClick={() => lastHandler(lastPage)} className={currentPage === lastPage ? style.nextButtonActive : style.nextButton}>Last</button>
+      </div>
+    </div>
+  </>
   )
 }
 
