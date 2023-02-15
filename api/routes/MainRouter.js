2a418c42f2ac5da5d8d1d02ca0728fa8b7d31fbf
akanashimi <113636928+akanashimi@users.noreply.github.com>
2023-02-15 17:11:13 -0600

    Pagination and News with MUI

--- api/routes/MainRouter.js
+++ api/routes/MainRouter.js
@@ -1,12 +1,11 @@
 const express = require("express")
 const router = express.Router()
 
-const userRoute = require("./src/userRoute");
-
-router.get("/",(req, res)=>{
-    res.send("Test") 
-})
+const userRoute = require("./user/userRouter");
+const authRouter = require("./auth/authRouter")
 
 router.use("/users", userRoute);
+router.use("/auth", authRouter);
+
 
 module.exports = router
