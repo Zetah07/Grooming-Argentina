6cb251c81eae42b6c13fca9cc5f80a74aee640b1
JuanRomeroSerna <juanromeroserna@gmail.com>
2023-02-14 21:11:37 -0500

    feat(simple auth and routes)

--- api/server.js
+++ api/server.js
@@ -15,11 +15,13 @@ app.use(cors(corsOptions));
 // for parsing json
 app.use(bodyParser.json());
 // for parsing application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.use(morgan("dev"));
 
 app.use('/', require("./routes/MainRouter"))
+app.use('/register', require("./routes/src/register"))
+app.use('/auth', require("./routes/src/auth"))
 
 mongoose.connection.once("open", () => {
   console.log("Connected to database")
