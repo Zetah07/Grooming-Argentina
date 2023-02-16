8f725ead8c14e64335bebbb55fbf4aca78019983
PatoProgramador <spartan1171947@hotmail.com>
2023-02-16 16:07:43 -0500

    fix the put of news

--- api/controllers/news/updateNews.js
+++ api/controllers/news/updateNews.js
@@ -1,26 +1,31 @@
 const mongoose  = require("mongoose");
 const news = require("../../models/news");
 
-//ID parser to objectID
-const parseID = (id) => {
-    return mongoose.Types.ObjectId(id);
-}
-
 const modifyNews = async (req, res) => {
-    const { id } = req.params.id;
+    const { id } = req.params;
+    // const {title, description, img, category, link, provinceOrLocation} = req.body;
     const body = req.body;
-    try {
-        news.updateOne(
-            {_id: parseID(id)},
-            body,
-            (err, docs) => {
-                res.status(200).send({items: docs})
-            }
-        )
-    } catch (error) {
-        res.status(400).json({"message": error})
-    }
 
+    news.updateOne(
+        {_id: id},
+        // {$set: {title: title, description: description, img: img, category: category, link: link, provinceOrLocation: provinceOrLocation}},
+        {$set: body},
+        (err, docs) => {
+            if(err) {
+                if(err.path === "_id") {
+                    res.status(400).json({"error": "The news doesn´t exist"})
+                } else {
+                    res.status(400).json({"error": err});
+                }
+            } else {
+                if(docs.matchedCount === 1) {
+                    res.status(200).json({"message": "Updated :)"});
+                } else {
+                    res.status(400).json({"message": "failed to update :("})
+                }
+            }
+        }
+    )
 };
 
-module.exports = { modifyNews }
+module.exports = { modifyNews };
