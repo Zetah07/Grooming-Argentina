916ee0261445b3821ae3685efb804105fed6ee2a
multichiki22 <myguelangel12@gmail.com>
2023-02-16 18:34:42 -0500

    CORS and credentials fix

--- api/middleware/credentials.js
+++ api/middleware/credentials.js
@@ -3,7 +3,7 @@ const allowOrigins = require("../config/allowOrigins")
 const credentials = (req,res,next)=>{
     const origin = req.headers.origin
     if ( allowOrigins.includes(origin)){
-        res.headers("Access-Control-Allow-Credentials", true)
+        res.header("Access-Control-Allow-Credentials", true)
     }
     next();
 }
