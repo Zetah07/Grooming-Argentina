8228372c68d41839eb584602def87e803d056ad9
Ronald <rormeas@gmail.com>
2023-02-16 12:56:56 -0500

    act_prueba

---
+++ client/src/components/Pages/NavBar2/NavBar2.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import s from './NavBar2.module.css'
+import { NavLink } from 'react-router-dom';
+
+
+const NavBar2 = () => {
+    return (
+        <div className={s.header}>
+            <div className={s.nav}>
+                <NavLink to='/' activeClassName={s.active} className={s.logo}><img src='./Grooming_Logo.png' alt="loga" /></NavLink>
+                <ul className={s.nav_menu}>
+                    <li className={s.nav_menu_item}>
+                        <NavLink to='/' activeClassName={s.active} >Home</NavLink>
+                    </li>
+                    <li className={s.nav_menu_item}>
+                        <NavLink to='/nosotros' activeClassName={s.active} >About</NavLink>
+                    </li>
+                    <li className={s.nav_menu_item}>
+                        <NavLink to='/contact' activeClassName={s.active} >Contact</NavLink>
+                    </li>
+                    <button className={s.btn_volunter}>Become Volunter</button>
+                </ul>
+
+            </div>
+        </div>
+    );
+};
+
+export default NavBar2;
