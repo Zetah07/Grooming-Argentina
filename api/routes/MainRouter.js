93c0577d8d324c5751cd7c49ef159b35269f9297
PatoProgramador <spartan1171947@hotmail.com>
2023-02-15 20:38:29 -0500

    Add controller and route for news

--- api/routes/MainRouter.js
+++ api/routes/MainRouter.js
@@ -2,10 +2,12 @@ const express = require("express")
 const router = express.Router()
 
 const userRoute = require("./user/userRouter");
-const authRouter = require("./auth/authRouter")
+const authRouter = require("./auth/authRouter");
+const newsRouter = require("./news/newsRouter")
 
 router.use("/users", userRoute);
 router.use("/auth", authRouter);
+router.use("/news", newsRouter);
 
 
 module.exports = router
