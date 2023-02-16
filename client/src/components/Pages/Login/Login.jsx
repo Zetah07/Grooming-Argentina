d340b985159dd99fb37a3a8b1603167b6dbe7a99
Zetah07 <sebastiancastro0792@gmail.com>
2023-02-16 18:00:47 -0500

    create and desing login

--- client/src/components/Pages/Login/Login.jsx
+++ client/src/components/Pages/Login/Login.jsx
@@ -1,6 +1,26 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import Button from 'react-bootstrap/Button';
+import Style from "./Login.module.css";
+import { BiUserCircle } from "react-icons/bi";
+import { RiLockPasswordLine } from "react-icons/ri";
+import { AiOutlineMail } from "react-icons/ai";
+
+function simulateNetworkRequest() {
+  return new Promise((resolve) => setTimeout(resolve, 2000));
+}
 
 const Login = () => {
+
+  const [isLoading, setLoading] = useState(false);
+
+  useEffect(() => {
+    if (isLoading) {
+      simulateNetworkRequest().then(() => {
+        setLoading(false);
+      });
+    }
+  }, [isLoading]);
+
   const [input, setInput] = useState({
     email: "",
     password: "",
@@ -60,44 +80,65 @@ const Login = () => {
     return formIsValid;
   };
 
+  const handleClick = () => setLoading(true);
+
   return (
-    <div className="login">
-      <form onSubmit={handleSubmit}>
-        <div className="form-group">
-          <label htmlFor="email">Email</label>
-          <input
-            type="email"
-            name="email"
-            id="email"
-            onChange={handleInputChange}
-            value={input.email}
-          />
-          {error.email ? <p>{error.email}</p> : null}
+    
+    <div className={Style.container}>
+      <div className={Style.loginContainer}>
+        <div className={Style.img}>
+          <BiUserCircle />
         </div>
-        <div className="form-group">
-          <label htmlFor="password">Password</label>
-          <input
-            type="password"
-            name="password"
-            id="password"
-            onChange={handleInputChange}
-            value={input.password}
-          />
-          {error.password ? <p>{error.password}</p> : null}
-        </div>
-        <div className="form-group">
-          <label htmlFor="confirmPassword">Confirm Password</label>
-          <input
-            type="password"
-            name="confirmPassword"
-            id="confirmPassword"
-            onChange={handleInputChange}
-            value={input.confirmPassword}
-          />
-          {error.confirmPassword ? <p>{error.confirmPassword}</p> : null}
-        </div>
-        <button className="button" type="submit">Login</button>
-      </form>
+        <form onSubmit={handleSubmit}>
+          <div class="input-group mb-3">
+          <span class="input-group-text"><AiOutlineMail/></span>
+            <input
+              class="form-control"
+              id="floatingInputGroup1"
+              type="email"
+              name="email"
+              placeholder="Enter email"
+              onChange={handleInputChange}
+              value={input.email}
+            />
+            <label for='floatingInputGroup1'></label>
+            {error.email ? <p>{error.email}</p> : null}
+          </div>
+          <div class="input-group mb-3">
+          <span class="input-group-text"><RiLockPasswordLine/></span>
+            <input
+              class="form-control"
+              id="floatingInputGroup2"
+              type="password"
+              name="password"
+              placeholder="Enter password"
+              onChange={handleInputChange}
+              value={input.password}
+            />
+            <label for='floatingInputGroup2'></label>
+            {error.password ? <p>{error.password}</p> : null}
+          </div>
+          <div class="input-group mb-3">
+          <span class="input-group-text"><RiLockPasswordLine/></span>
+            <input
+              class="form-control"
+              id="floatingInputGroup3"
+              type="password"
+              name="confirmPassword"
+              placeholder="Confirm password"
+              onChange={handleInputChange}
+              value={input.confirmPassword}
+            />
+            <label for='floatingInputGroup3'></label>
+            {error.confirmPassword ? <p>{error.confirmPassword}</p> : null}
+          </div>
+          <Button disabled={isLoading}
+      onClick={!isLoading ? handleClick : null}
+    >
+      {isLoading ? 'Loading…' : 'Login'}
+          </Button>
+        </form>
+      </div>
     </div>
   );
 };
