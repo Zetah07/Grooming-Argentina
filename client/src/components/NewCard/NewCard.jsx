ae76cb8736c7a1368a16cf1b1fdeb2ea3cb8544f
akanashimi <113636928+akanashimi@users.noreply.github.com>
2023-02-16 12:53:58 -0600

    News and pagination

--- client/src/components/NewCard/NewCard.jsx
+++ client/src/components/NewCard/NewCard.jsx
@@ -1,23 +1,36 @@
-import { Box, Card, CardContent, Typography, CardActions, Button, CardMedia } from "@mui/material"
+import React from "react";
+import { Card, CardContent, Typography, CardActions, Button, CardMedia, CardActionArea, Grid } from "@mui/material"
 
-const NewCard = (props) => {
-    return <Box width='400px'>
-        <Card>
-            <CardMedia component='img' height='180px' image={props.image} alt={props.title} />
-            <CardContent>
-                <Typography gutterBottom variant='h5' component='div'>
-                    {props.title}
-                </Typography>
-                <Typography variant='body2' color='text.secondary'>
-                    {props.description}
-                </Typography>
-            </CardContent>
-            <CardActions>
-                <Button size='small'>Compartir</Button>
-                <Button size='small'>Leer más...</Button>
-            </CardActions>
+const NewCard = ({ image, title, description, date, categories }) => {
+    return (<Grid item xs={5} md={5} lg={5}>
+        <Card sx={{ maxWidth: 345 }}>
+            <CardActionArea>
+                <CardMedia component='img' height='250' image={image} alt={title} />
+                <CardContent>
+                    <Typography variant='p' component='div'>
+                        Categorias:
+                    </Typography>
+                    {categories.map(category => {
+                        return <Typography variant='p' component='div'>
+                            {category}
+                        </Typography>
+                    })}
+                    <Typography gutterBottom variant='h4' component='div'>
+                        {title}
+                    </Typography>
+                    <Typography variant='p' component='div'>
+                        Publicado:
+                    </Typography>
+                    <Typography variant='p' component='div'>
+                        {date}
+                    </Typography>
+                </CardContent>
+                <CardActions>
+                    <Button size='small' variant='text' color='primary' href="https://www.google.com">Leer mas...</Button>
+                </CardActions>
+            </CardActionArea>
         </Card>
-    </Box>
+    </Grid>)
 }
 
 export default NewCard;
