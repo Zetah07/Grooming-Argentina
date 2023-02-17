cdcfe6f8988e307bba23d8c7fac24cc7d409666a
Zetah07 <sebastiancastro0792@gmail.com>
2023-02-17 12:35:43 -0500

    open axios in index.js // fix login number 2 in construction

--- client/src/Redux/Reducer/index.js
+++ client/src/Redux/Reducer/index.js
@@ -1,36 +1,21 @@
-
 /* eslint-disable no-unused-vars */
-import {
-    GET_ALL_NEWS,
-    //     GET_ALL_CATEGORIES,
-    //     GET_ALL_NEWS_BY_CATEGORY,
-    //     GET_ALL_NEWS_BY_ID,
-    //     ADD_NEWS, 
-    //     UPDATE_NEWS, //tener en cuenta
-    //     DELETE_NEWS, // tener en cuenta 
-    //     GET_ALL_USERS,
-    //     GET_ALL_VOLUNTEER,
-    //     COURSES_BY_VOLUNTEER,
-    //     ADD_COURSE,
-    //     UPDATE_COURSE, //tener en cuenta 
-    //     DELETE_COURSE, //tener en cuenta
-    //     GET_ALL_COURSES,
-    //     GET_ALL_COURSES_BY_ID,
-    //     GET_ALL_COURSES_BY_CATEGORY,
-    //     FILTERS
-} from '../Actions'
+import { GET_ALL_NEWS, GET_LOGIN, ERROR } from "../Actions";
 
 const initialState = {
-    news: [],
-}
+  news: [],
+};
 
 const rootReducer = (state = initialState, action) => {
-    switch (action.type) {
-        case GET_ALL_NEWS:
-            return { ...state, news: action.payload }
-        default:
-            return { ...state };
-    }
-}
+  switch (action.type) {
+    case GET_ALL_NEWS:
+      return { ...state, news: action.payload };
+    case GET_LOGIN:
+      return { ...state, user: action.payload };
+    case ERROR:
+      return { ...state, error: action.payload };
+    default:
+      return { ...state };
+  }
+};
 
 export default rootReducer;
