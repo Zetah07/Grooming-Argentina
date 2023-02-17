5afca0a5a320514401f51737c178f0e082bfe03c
multichiki22 <myguelangel12@gmail.com>
2023-02-17 14:55:40 -0500

    Added order query in courses route

--- api/controllers/courses/readCoursesController.js
+++ api/controllers/courses/readCoursesController.js
@@ -1,11 +1,27 @@
 const courses = require("../../models/courses")
 
 const handleCourses = async(req, res) =>{
+    const {order} = req.query
     try {
         const userCoursesList =  await courses.find({})
-        res.status(200).json(userCoursesList)
+        if (order) {
+            if (order === "descending"){
+                const orderedUserCoursesList = userCoursesList.sort(function(a,b){
+                    return new Date(b.createdAt) - new Date(a.createdAt)
+                 })
+                 return res.status(200).json(orderedUserCoursesList)
+            }else if (order === "ascending"){
+                const orderedUserCoursesList = userCoursesList.sort(function(a,b){
+                    return   new Date(a.createdAt) - new Date(b.createdAt)
+                 })
+                 return res.status(200).json(orderedUserCoursesList)
+            }else{
+               return res.status(200).json(userCoursesList)
+            }
+        }
+        return res.status(200).json(userCoursesList)
     } catch (error) {
-        res.status(400).json({message:error.message})
+        return res.status(400).json({message:error.message})
     }
 }
 module.exports = handleCourses
