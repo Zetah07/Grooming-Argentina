8f959459fb07f48280abef59b7f5852ca3a81f4a
PatoProgramador <spartan1171947@hotmail.com>
2023-02-16 20:04:36 -0500

    Add filters

--- api/controllers/news/getNews.js
+++ api/controllers/news/getNews.js
@@ -1,10 +1,25 @@
 const news = require("../../models/news");
 
 const getNews = async (req, res) => {
+    const { categoria, provinciaOLocacion } = req.query;
+    let filteredNews;
+
     try {
-        news.find((err, docs) => {
-            res.status(200).json({docs});
-        })
+        if(categoria && provinciaOLocacion) {
+            filteredNews = await news.find({category: categoria, provinceOrLocation: provinciaOLocacion}).exec();
+            res.status(200).json(filteredNews);
+        }
+        else if(categoria) {
+            filteredNews = await news.find({category: categoria}).exec();
+            res.status(200).json(filteredNews);
+        }
+        else if(provinciaOLocacion) {
+            filteredNews = await news.find({provinceOrLocation: provinciaOLocacion}).exec();
+            res.status(200).json(filteredNews);
+        } else {
+            filteredNews = await news.find({});
+            res.status(200).json(filteredNews);
+        }
     } catch (error) {
         res.status(500).json({"message": error});
     }
