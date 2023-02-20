da5877f1378b197b6894757fb7499c9d6cc27126
akanashimi <113636928+akanashimi@users.noreply.github.com>
2023-02-19 23:28:31 -0600

    Form responsive and validate, new details view, redux actions get by id,

--- client/src/Redux/Actions/index.js
+++ client/src/Redux/Actions/index.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 export const GET_ALL_NEWS = "GET_ALL_NEWS";
+export const GET_NEW_BY_ID = "GET_NEW_BY_ID";
 export const GET_LOGIN = "GET_LOGIN";
 export const ERROR = "ERROR";
 
@@ -8,6 +9,18 @@ export const getAllNews = () => {
     const news = await axios.get("http://localhost:3500/news/");
     if (news.data) {
       dispatch({ type: GET_ALL_NEWS, payload: news.data });
+    } else {
+      dispatch({ type: ERROR });
+    }
+  };
+};
+
+export const getNewByID = (id) => {
+  console.log(id)
+  return async function (dispatch) {
+    const newID = await axios.get(`http://localhost:3500/news/?id=${id}`);
+    if (newID.data) {
+      dispatch({ type: GET_NEW_BY_ID, payload: newID.data });
     } else {
       dispatch({ type: ERROR });
     }
