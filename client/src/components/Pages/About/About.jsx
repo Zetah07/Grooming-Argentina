7658959e8f03e4717ae7a2412ac106ce56530183
PatoProgramador <spartan1171947@hotmail.com>
2023-02-21 10:04:24 -0500

    CSS Beta of about

--- client/src/components/Pages/About/About.jsx
+++ client/src/components/Pages/About/About.jsx
@@ -1,14 +1,13 @@
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-// import Banner from '../../../assets/c387.jpg';
 import imgUs from '../../../assets/CC42AH4J2RDO3JQ34RN6JDKOKA.jpg'
 import s from './About.module.css'
-import Footer from '../../Footer/Footer';
+
 
 const About = () => {
   return (
-    <Container>
+    <Container className={s.container}>
 
       <Row>
         <Col className={s.banner}>
@@ -16,12 +15,11 @@ const About = () => {
         </Col>
       </Row>
 
-      <Row>
-        <Col>
+      <Row className={s.container2}>
+        <Col className={s.imgContainer}>
           <img src={imgUs} alt="img_us" />
         </Col>
-        <Col>
-          <div>
+        <Col className={s.textContainer}>
             <h4>¿QUIÉNES SOMOS?</h4>
             <p text='end'>
               Grooming Argentina nació Institucionalmente en el año 2014 y se convirtió en la primera Organización global creada para combatir el delito de «grooming o child grooming». Fue originada con el propósito de trabajar fundamentalmente sobre ejes basados en la prevención y concientización en pos de la erradicación del grooming en Argentina y América Latina.
@@ -29,10 +27,8 @@ const About = () => {
               A su vez, cuenta con la novedosa app de denuncias «GAPP» gratuita y de alcance global, que permite denunciar el delito con tan solo «presionar un botón».
               Grooming Argentina se encuentra a la vanguardia en materia de promoción y protección de los derechos de niños, niñas y adolescentes en el ecosistema de Internet.
             </p>
-          </div>
         </Col>
       </Row>
-      <Footer />
     </Container>
   );
 }
