d2971cdb1ddcf5b7bca20ceecb3ca97511222b27
JuanRomeroSerna <juanromeroserna@gmail.com>
2023-02-21 09:50:50 -0500

    pato fix all

--- client/src/components/News/News.jsx
+++ client/src/components/News/News.jsx
@@ -1,18 +1,17 @@
-import React from 'react'
+import React from "react";
 import PaginationNews from "../Pages/PaginationNews/PaginationNews.jsx";
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import NewCard from '../NewCard/NewCard';
-import SearchBar from '../Pages/SeachBar/SearchBar.jsx';
-import { getAllNews, resetFilter } from '../../Redux/Actions/index.js';
+import NewCard from "../NewCard/NewCard";
+import SearchBar from "../Pages/SeachBar/SearchBar.jsx";
+import { getAllNews, resetFilter } from "../../Redux/Actions/index.js";
 import s from "./News.module.css";
-import Button from 'react-bootstrap/Button';
+import Button from "react-bootstrap/Button";
 
 const News = () => {
-
   const dispatch = useDispatch();
-  const newspaper = useSelector(state => state.news);
-  const filter = useSelector(state => state.filter);
+  const newspaper = useSelector((state) => state.news);
+  const filter = useSelector((state) => state.filter);
   const newsPerPage = 6;
   const pageNumberLimit = 5;
   const totalNews = newspaper.length;
@@ -20,7 +19,8 @@ const News = () => {
   const [currentPage, setCurrentPage] = useState(0);
   const [minPageNumberLimit, setminPageNumberLimit] = useState(0);
   const [maxPageNumberLimit, setmaxPageNumberLimit] = useState(5);
-  if (newspaper.length > 0 && items.length === 0) setItems([...newspaper].splice(0, newsPerPage));
+  if (newspaper.length > 0 && items.length === 0)
+    setItems([...newspaper].splice(0, newsPerPage));
 
   useEffect(() => {
     dispatch(getAllNews());
@@ -42,7 +42,7 @@ const News = () => {
     setCurrentPage(firstPage);
     setminPageNumberLimit(0);
     setmaxPageNumberLimit(5);
-  }
+  };
 
   const prevHandler = () => {
     const prevPage = currentPage - 1;
@@ -50,11 +50,11 @@ const News = () => {
     if (prevPage < 0) return;
     setItems([...newspaper].splice(firstIndex, newsPerPage));
     setCurrentPage(prevPage);
-    if ((currentPage) % pageNumberLimit === 0) {
+    if (currentPage % pageNumberLimit === 0) {
       setmaxPageNumberLimit(maxPageNumberLimit - pageNumberLimit);
       setminPageNumberLimit(minPageNumberLimit - pageNumberLimit);
     }
-  }
+  };
 
   const nextHandler = () => {
     const nextPage = currentPage + 1;
@@ -66,7 +66,7 @@ const News = () => {
       setmaxPageNumberLimit(maxPageNumberLimit + pageNumberLimit);
       setminPageNumberLimit(minPageNumberLimit + pageNumberLimit);
     }
-  }
+  };
 
   const lastHandler = (lastPage) => {
     const firstIndex = lastPage * newsPerPage;
@@ -74,57 +74,67 @@ const News = () => {
     setCurrentPage(lastPage);
     setmaxPageNumberLimit(5 * Math.ceil(lastPage / 5));
     setminPageNumberLimit(5 * Math.floor(lastPage / 5));
-  }
+  };
 
   const pages = (numberPage) => {
     const firstIndex = numberPage * newsPerPage;
     setItems([...newspaper].splice(firstIndex, newsPerPage));
     setCurrentPage(numberPage);
-  }
+  };
 
   // const handleChange = (event) => {
   //   console.log(event);
   // }
 
-  return (<>
-    <div className={s.container1}>
-      <span>Noticias</span>
-    </div>
-    <br />
-    <div class="container">
-      <section class="row pb-3">
-        <article class="row g-3 col-12 col-md-4 order-md-1 col-lg-4 order-lg-1"><SearchBar /></article>
-        <article class="row g-3 col-12 col-md-8 col-lg-8 ">
-          {items.map(paper => {
-            return <NewCard
-              key={paper._id}
-              id={paper._id}
-              title={paper.title}
-              image={paper.img}
-              description={paper.description}
-              createdAt={paper.createdAt}
-              category={paper.category}
-            />
-          })}
-        </article>
-      </section>
-      <div className="row offset-2">
-        <PaginationNews totalNews={totalNews}
-          firstHandler={firstHandler}
-          prevHandler={prevHandler}
-          nextHandler={nextHandler}
-          lastHandler={lastHandler}
-          pages={pages}
-          newsPerPage={newsPerPage}
-          currentPage={currentPage}
-          pageNumberLimit={pageNumberLimit}
-          maxPageNumberLimit={maxPageNumberLimit}
-          minPageNumberLimit={minPageNumberLimit} />
+  return (
+    <>
+      <div className={s.container1}>
+        <span>Noticias</span>
       </div>
-    </div>
-    <br />
-    <Button variant="primary" href="/crearnoticia">Crear Noticia</Button>
-  </>)
-}
+      <br />
+      <div class="container">
+        <section class="row pb-3">
+          <article class="row g-3 col-12 col-md-12 col-lg-4 order-lg-1">
+            <SearchBar />
+          </article>
+          <article class="row g-3 col-12 col-md-12 col-lg-8 ">
+            {items.map((paper) => {
+              return (
+                <NewCard
+                  key={paper._id}
+                  id={paper._id}
+                  title={paper.title}
+                  image={paper.img}
+                  description={paper.description}
+                  createdAt={paper.createdAt}
+                  category={paper.category}
+                />
+              );
+            })}
+          </article>
+        </section>
+        <div className="row offset-2">
+          <PaginationNews
+            totalNews={totalNews}
+            firstHandler={firstHandler}
+            prevHandler={prevHandler}
+            nextHandler={nextHandler}
+            lastHandler={lastHandler}
+            pages={pages}
+            newsPerPage={newsPerPage}
+            currentPage={currentPage}
+            pageNumberLimit={pageNumberLimit}
+            maxPageNumberLimit={maxPageNumberLimit}
+            minPageNumberLimit={minPageNumberLimit}
+          />
+        </div>
+      </div>
+      <br />
+      <Button variant="primary" href="/crearnoticia">
+        Crear Noticia
+      </Button>
+    </>
+  );
+};
 
-export default News
+export default News;
