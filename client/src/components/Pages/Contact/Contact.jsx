262077787dec9bec195f4fc0422e013acdcb190f
JuanRomeroSerna <juanromeroserna@gmail.com>
2023-02-21 07:39:32 -0500

    contact page rdy and download report button

--- client/src/components/Pages/Contact/Contact.jsx
+++ client/src/components/Pages/Contact/Contact.jsx
@@ -1,47 +1,100 @@
-/* eslint-disable react/jsx-no-target-blank */
-import React, {useRef} from 'react'
-import './Contact.module.css'
-import emailjs from '@emailjs/browser';
+import React, { useRef } from "react";
+import { Form, Button } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
+import emailjs from "@emailjs/browser";
+import style from "./Contact.module.css";
 
-const {
-  EMAIL_SERVICE, EMAIL_TEMPLATE, EMAIL_USER,
-} = process.env;
+const { EMAIL_SERVICE, EMAIL_TEMPLATE, EMAIL_USER } = process.env;
 
 const Contact = () => {
   const form = useRef();
 
-  const sendEmail =(e) =>{
-    e.preventDefault();
+  const sendEmail = (event) => {
+    event.preventDefault();
 
-    emailjs.sendForm(EMAIL_SERVICE,EMAIL_TEMPLATE, form.current,EMAIL_USER )
-    e.target.reset()
-  }
+    emailjs.sendForm(EMAIL_SERVICE, EMAIL_TEMPLATE, form.current, EMAIL_USER);
+    event.target.reset();
+  };
+
   return (
-    <section id='contact'>
-      <h5>Ponerse en contacto</h5>
-      <h2>Contactame</h2>
-      <div className='contact-container'>
-        <div className='contact-options'>
-          <article className='contact-option'>
-            <h3>Email</h3>
-            <a href='mailto:contacto@groomingarg.org'target='_blank' >Envia el mensaje</a>
+    <section id="contact">
+      <h2 className={style.title}>Ponerse en contacto</h2>
+      <div className={style.contactContainer}>
+        <div className={style.contact_container}>
+          <article className={style.contact_option}>
+            <h3 className={style.contact_link}>Email:</h3>
+            <a
+              href="mailto:contacto@groomingarg.org"
+              target="_blank"
+              rel="noopener noreferrer"
+              className={style.icon}
+            >
+              <i className="bi bi-envelope-at"></i>
+            </a>
           </article>
 
-          <article className='contact-option'>
-            <h3>WhatsApp</h3>
-            <a href='https://wa.me/+5491124811722' target='_blank' >WhatsApp</a>
+          <article className={style.contact_option}>
+            <h3 className={style.contact_link}>WhatsApp:</h3>
+            <a
+              href="https://wa.me/+5491124811722"
+              target="_blank"
+              rel="noopener noreferrer"
+              className={style.icon}
+            >
+              <i className="bi bi-whatsapp"></i>
+            </a>
           </article>
         </div>
-        <form ref={form} onSubmit={sendEmail}>
-          <input type='text' name='' placeholder='Escribe tu nombre completo' required />
-          <input type='email' name='email' placeholder='Escribe tu email' required />
-          <textarea name='message' rows='15' placeholder='Escribe tu mensaje' required />
-          <button type='submit'>Enviar</button>
-        </form>
       </div>
+      <Form ref={form} onSubmit={sendEmail} className={style.contactForm}>
+        <Form.Group controlId="formName">
+          <Form.Label className={style.form_label}>Nombre completo:</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Escribe tu nombre completo"
+            required
+            className={style.formControl}
+          />
+        </Form.Group>
+
+        <Form.Group controlId="formEmail">
+          <Form.Label className={style.form_label}>Email:</Form.Label>
+          <Form.Control
+            type="email"
+            placeholder="Escribe tu email"
+            required
+            className={style.formControl}
+          />
+        </Form.Group>
+
+        <Form.Group controlId="formSubject">
+          <Form.Label className={style.form_label}>Asunto:</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Escribe el asunto"
+            required
+            className={style.formControl}
+          />
+        </Form.Group>
+
+        <Form.Group controlId="formMessage">
+          <Form.Label className={style.form_label}>Mensaje:</Form.Label>
+          <Form.Control
+            as="textarea"
+            rows={5}
+            placeholder="Escribe tu mensaje"
+            required
+            className={style.formControl}
+          />
+        </Form.Group>
+
+        <Button variant="primary" type="submit" className={style.submitButton}>
+          Enviar
+        </Button>
+      </Form>
     </section>
+  );
+};
 
-  )
-}
-
-export default Contact
+export default Contact;
