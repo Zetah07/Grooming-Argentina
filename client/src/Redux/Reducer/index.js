9d6f37c3f39db89d0f9db402e083e3da0d1e5fd5
akanashimi <113636928+akanashimi@users.noreply.github.com>
2023-02-23 11:25:18 -0600

    View Blog, Card with information of BackEnd, Filter By Title functional, Pagination Functional

--- client/src/Redux/Reducer/index.js
+++ client/src/Redux/Reducer/index.js
@@ -1,9 +1,21 @@
 /* eslint-disable no-unused-vars */
-import { GET_ALL_NEWS, GET_NEW_BY_ID, GET_NEWS_BY_TITLE, GET_LOGIN, ERROR, RESET_FILTER } from "../Actions";
+import {
+  GET_ALL_NEWS,
+  GET_ALL_BLOGS,
+  GET_NEW_BY_ID,
+  GET_BLOG_BY_ID,
+  GET_NEWS_BY_TITLE,
+  GET_BLOGS_BY_TITLE,
+  GET_LOGIN,
+  ERROR,
+  RESET_FILTER,
+} from "../Actions";
 
 const initialState = {
   news: [],
   newID: [],
+  blogs: [],
+  blogId: [],
   filter: false
 };
 
@@ -11,10 +23,16 @@ const rootReducer = (state = initialState, action) => {
   switch (action.type) {
     case GET_ALL_NEWS:
       return { ...state, news: action.payload, filter: true };
+    case GET_ALL_BLOGS:
+      return { ...state, blogs: action.payload, filter: true };
     case GET_NEW_BY_ID:
       return { ...state, newID: action.payload };
+    case GET_BLOG_BY_ID:
+      return { ...state, blogId: action.payload };
     case GET_NEWS_BY_TITLE:
       return { ...state, news: action.payload, filter: true };
+    case GET_BLOGS_BY_TITLE:
+      return { ...state, blogs: action.payload, filter: true };
     case GET_LOGIN:
       return { ...state, user: action.payload };
     case ERROR:
---
+++ client/src/components/Blog/Blog.jsx
@@ -0,0 +1,133 @@
+import React from 'react';
+import { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getAllBlogs, resetFilter } from '../../Redux/Actions';
+import PaginationComp from "../Pages/PaginationComp/PaginationComp.jsx";
+import s from "./Blog.Module.css";
+import SearchBar from "../Pages/SeachBar/SearchBar.jsx";
+import Button from "react-bootstrap/Button";
+import { BlogCard } from '../BlogCard/BlogCard';
+
+const Blog = () => {
+  const dispatch = useDispatch();
+  const blogs = useSelector(state => state.blogs);
+  const filter = useSelector(state => state.filter);
+  const blogsPerPage = 10;
+  const pageNumberLimit = 5;
+  const totalBlogs = blogs.length;
+  const [items, setItems] = useState([]);
+  const [currentPage, setCurrentPage] = useState(0);
+  const [minPageNumberLimit, setminPageNumberLimit] = useState(0);
+  const [maxPageNumberLimit, setmaxPageNumberLimit] = useState(5);
+  if (blogs.length > 0 && items.length === 0) setItems([...blogs].splice(0, blogsPerPage));
+  console.log(blogs);
+
+  useEffect(() => {
+    dispatch(getAllBlogs());
+  }, [dispatch]);
+
+  useEffect(() => {
+    if (filter === true) {
+      setCurrentPage(0);
+      setmaxPageNumberLimit(5);
+      setminPageNumberLimit(0);
+      setItems([...blogs].splice(0, blogsPerPage));
+      dispatch(resetFilter());
+    }
+  }, [dispatch, filter, blogs]);
+
+  const firstHandler = (firstPage) => {
+    const firstIndex = firstPage * blogsPerPage;
+    setItems([...blogs].splice(firstIndex, blogsPerPage));
+    setCurrentPage(firstPage);
+    setminPageNumberLimit(0);
+    setmaxPageNumberLimit(5);
+  };
+
+  const prevHandler = () => {
+    const prevPage = currentPage - 1;
+    const firstIndex = prevPage * blogsPerPage;
+    if (prevPage < 0) return;
+    setItems([...blogs].splice(firstIndex, blogsPerPage));
+    setCurrentPage(prevPage);
+    if (currentPage % pageNumberLimit === 0) {
+      setmaxPageNumberLimit(maxPageNumberLimit - pageNumberLimit);
+      setminPageNumberLimit(minPageNumberLimit - pageNumberLimit);
+    }
+  };
+
+  const nextHandler = () => {
+    const nextPage = currentPage + 1;
+    const firstIndex = nextPage * blogsPerPage;
+    if (firstIndex > totalBlogs) return;
+    setItems([...blogs].splice(firstIndex, blogsPerPage));
+    setCurrentPage(nextPage);
+    if (currentPage + 2 > maxPageNumberLimit) {
+      setmaxPageNumberLimit(maxPageNumberLimit + pageNumberLimit);
+      setminPageNumberLimit(minPageNumberLimit + pageNumberLimit);
+    }
+  };
+
+  const lastHandler = (lastPage) => {
+    const firstIndex = lastPage * blogsPerPage;
+    setItems([...blogs].splice(firstIndex, blogsPerPage));
+    setCurrentPage(lastPage);
+    setmaxPageNumberLimit(5 * Math.ceil(lastPage / 5));
+    setminPageNumberLimit(5 * Math.floor(lastPage / 5));
+  };
+
+  const pages = (numberPage) => {
+    const firstIndex = numberPage * blogsPerPage;
+    setItems([...blogs].splice(firstIndex, blogsPerPage));
+    setCurrentPage(numberPage);
+  };
+
+  return (<>
+    <div className={s.container1}>
+      <span>Blogs</span>
+    </div>
+    <br />
+    <div class="container">
+      <section class="row pb-3">
+        <article class="row g-3 col-12 col-md-12 col-lg-4 order-lg-1">
+          <SearchBar />
+        </article>
+        <article class="row g-3 col-12 col-md-12 col-lg-8 ">
+          {items.map((blog) => {
+            return (
+              <BlogCard
+                key={blog._id}
+                id={blog._id}
+                author={blog.author}
+                title={blog.title}
+                content={blog.content}
+                createdAt={blog.createdAt}
+              />
+            );
+          })}
+        </article>
+      </section>
+      <div className="row offset-2">
+        <PaginationComp
+          totalItems={totalBlogs}
+          firstHandler={firstHandler}
+          prevHandler={prevHandler}
+          nextHandler={nextHandler}
+          lastHandler={lastHandler}
+          pages={pages}
+          itemsPerPage={blogsPerPage}
+          currentPage={currentPage}
+          pageNumberLimit={pageNumberLimit}
+          maxPageNumberLimit={maxPageNumberLimit}
+          minPageNumberLimit={minPageNumberLimit}
+        />
+      </div>
+    </div>
+    <br />
+    <Button variant="primary" href="/crearblog">
+      Crear Blog
+    </Button>
+  </>)
+}
+
+export default Blog
