c03ee252f0b480b059077be7e575bb4127a74161
JonathanG0m3z <104044651+JonathanG0m3z@users.noreply.github.com>
2023-02-23 14:42:03 -0500

    Students video player

---
+++ client/src/components/Students/Students.jsx
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+import ReactPlayer from 'react-player';
+import './students.css';
+
+export default function Students(){
+    const [title, setTitle] = useState('');
+
+    const handleReady = (player)=>{
+        setTitle(player.getInternalPlayer().element.title);
+    };
+    return(
+        <>
+        <div className='player-wrapper'>
+            <ReactPlayer className='react-player'
+                url="https://vimeo.com/798384070?access_token="
+                controls 
+                width='98%'
+                height='100%'
+                onReady={handleReady}
+            />
+            
+        </div>
+        <h1 className="title">{title}</h1>
+        </>
+        
+            
+    )
+};
