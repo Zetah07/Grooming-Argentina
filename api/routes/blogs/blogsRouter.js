715742d2db7c1f41b39199ebcf8863c18ca8ac28
JuanRomeroSerna <juanromeroserna@gmail.com>
2023-02-24 17:39:15 -0500

    feat(createbloge implemented and secure with token

--- api/routes/blogs/blogsRouter.js
+++ api/routes/blogs/blogsRouter.js
@@ -4,13 +4,14 @@ const { getPosts, getPostById } = require("../../controllers/blog/getBlogControl
 const createBlogController = require("../../controllers/blog/createBlogController")
 const updateBlogController = require("../../controllers/blog/updateBlogController")
 const deleteBlogController = require("../../controllers/blog/deleteBlogController")
+const verifyRoles = require("../../middleware/verifyRoles");
 
 const router = express.Router();
 
 router.get('/', getPosts);
 router.get('/:id', getPostById);
-router.post('/', veryfyJWT, createBlogController);
-router.put('/:id', veryfyJWT, updateBlogController);
-router.delete('/:id', veryfyJWT, deleteBlogController);
+router.post('/', veryfyJWT, verifyRoles(["user", "admin", "hr", "volunteer", "editor"]), createBlogController);
+router.put('/:id', veryfyJWT, verifyRoles(["user", "admin", "hr", "volunteer", "editor"]), updateBlogController);
+router.delete('/:id', veryfyJWT, verifyRoles(), deleteBlogController);
 
 module.exports = router;
