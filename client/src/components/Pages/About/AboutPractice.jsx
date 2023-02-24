f82b28fed8919e194aef208307c7a774754546c0
PatoProgramador <spartan1171947@hotmail.com>
2023-02-24 18:20:20 -0500

    Team cards component

---
+++ client/src/components/Pages/About/AboutPractice.jsx
@@ -0,0 +1,52 @@
+const team = [
+    {
+        id: 1,
+        name: "Chris Patt",
+        avatar: "",
+        rol: "CEO/Founder"
+    },
+    {
+        id: 2,
+        name: "Mike Carter",
+        avatar: "",
+        rol: "Manager"
+    },
+    {
+        id: 3,
+        name: "Bella Crusio",
+        avatar: "",
+        rol: "Volunteer"
+    },
+    {
+        id: 4,
+        name: "Chris Patt",
+        avatar: "",
+        rol: "CEO/Founder"
+    },
+    {
+        id: 5,
+        name: "Chris Patt",
+        avatar: "",
+        rol: "CEO/Founder"
+    },
+    {
+        id: 6,
+        name: "Chris Patt",
+        avatar: "",
+        rol: "CEO/Founder"
+    },
+    {
+        id: 7,
+        name: "Chris Patt",
+        avatar: "",
+        rol: "CEO/Founder"
+    },
+    {
+        id: 8,
+        name: "Chris Patt",
+        avatar: "",
+        rol: "CEO/Founder"
+    }
+];
+
+export default team;
