1d9ac7519ade95c3402ec9dd048d8149dee1ef9e
PatoProgramador <spartan1171947@hotmail.com>
2023-02-25 13:38:30 -0500

    Cards responsive

--- client/src/components/Pages/About/TeamCards/Card.jsx
+++ client/src/components/Pages/About/TeamCards/Card.jsx
@@ -2,12 +2,14 @@ import s from "./CardTeam.module.css"
 
 const TeamCard = ({name, avatar, rol}) => {
     return (
-        <div class="col-sm-12 col-md-6 col-lg-3 col-xl-3">
-            <div class="card h-100 w-100">
-                <img src={avatar} alt="avatar" class="card-img-top h-50"/>
+        <div class="col-sm-12 col-md-6 col-lg-4 col-xl-3">
+            <div className={s.container}>
+                <img className={s.avatarIMG} src={avatar} alt="avatar"/>
                 <div class="card-body">
-                    <h2 class="card-title">{name}</h2>
-                    <span class="card-subtitle mb-2 text-muted">{rol}</span>
+                    <div className={s.bodycss}>
+                        <h2 class="card-title">{name}</h2>
+                        <span class="card-subtitle mb-2 text-muted">{rol}</span>
+                    </div>
                 </div>
             </div>
         </div>
