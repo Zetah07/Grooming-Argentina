f162ac2f3b6a31031ad38de6b5feac1363396c75
PatoProgramador <spartan1171947@hotmail.com>
2023-02-25 11:26:22 -0500

    Vision responsive

--- client/src/components/Pages/About/About.jsx
+++ client/src/components/Pages/About/About.jsx
@@ -78,44 +78,46 @@ const About = () => {
         </div>
       </div>
       {/*Vision*/}
-      <div className={s.videoContainer}>
-        <div className={s.visionDiv}>
-          <h4 className={s.visionTitle}>Nuestra visión</h4>
-          <h3>Trabajar y ayudar a las personas en áreas y tiempos de calamidades nacionales.</h3>
-          <p>
-            India es la tierra donde las calamidades naturales visitan a menudo y sin previo aviso. Incluso con este historial,
-            el país no cuenta con un equipo de respuesta rápida o suministros para hacer frente a las consecuencias. Maatram tiene
-              la intención de cerrar esta brecha mediante la creación de un equipo de respuesta para áreas sensibles.
-          </p>
-          <h3>Planes de autoempleo y alivio de la pobreza.</h3>
-          <p>
-          La autosuficiencia es la verdadera independencia. Ayudamos a las personas a ser autosuficientes brindándoles habilidades
-          y ayudándolas a encontrar empleo o un medio para trabajar por cuenta propia.
-          </p>
-          <h3>Mejora social y económica para los desfavorecidos y oprimidos a través de la educación, la vivienda, la vivienda,
-             la organización y la facilitación de microayudas financieras a corto y largo plazo.
-          </h3>
-          <p>
-          Tenemos la intención de cerrar la brecha de la inequidad proporcionando educación, vivienda, vivienda y ayudas 
-          microfinancieras a corto y largo plazo a los pobres. Solo ayudándolos a volverse autosuficientes podemos lograr 
-          verdaderamente un maatram revolucionario social y económicamente.
-          </p>
-          <h3>Empoderamiento de la Mujer y Derechos Humanos.</h3>
-          <p>
-          Estamos en el siglo XXI y todavía no se ha avanzado en el campo del empoderamiento de la mujer y los derechos humanos.
-           Culpe a quien quiera, pero nosotros, que creemos en la reparación inmediata de los errores cometidos, ya estamos presionando
-            por el empoderamiento de las mujeres y la práctica del respeto de los derechos humanos básicos de uno mismo y de los demás.
-          </p>
-          <h3>Rehabilitación de Minusválidos, Indigentes, Viudas, Huérfanos, Geriatría, Desfavorecidos y Pacientes Crónicos (VIH/SIDA/Cáncer/T.B.etc.)</h3>
-          <p>To promote and organize self help groups for Men and Women.</p>
-          <h3>Abolición del trabajo infantil y Establecimiento de los derechos del niño.</h3>
-          <p>La pobreza ha engendrado una sociedad donde florece el trabajo infantil. Lo vemos en todas partes. Es un espectáculo tan común que pasa desapercibido,
-             incluso cuando nos mira a la cara. Es una cosa peligrosa, este punto al que hemos sido llevados, tan insensible que esta visión cotidiana de niños pequeños
-              cargando con la carga de un adulto, en un momento en que sus manos, pies y mentes deberían estar ocupados en diversión infantil, retozar. y el aprendizaje,
-               evoca poca o ninguna respuesta. Estamos tratando de generar más conciencia sobre un mal social que no debería necesitar tal esfuerzo pero que lamentablemente 
-               lo necesita. Estamos tratando de dar oportunidades de vida a estos niños atrapados en el círculo social de la pobreza, el abuso del alcohol y el analfabetismo.
-                Estamos trabajando en el campo de los derechos del niño que es un paraguas más amplio y busca poner la infancia al alcance de los niños desfavorecidos.
-          </p>
+      <div class="container-fluid" className={s.videoContainer}>
+        <div class="row" className={s.visionDiv}>
+          <div class="col" >
+            <h4 className={s.visionTitle}>Nuestra visión</h4>
+            <h3>Trabajar y ayudar a las personas en áreas y tiempos de calamidades nacionales.</h3>
+            <p>
+              India es la tierra donde las calamidades naturales visitan a menudo y sin previo aviso. Incluso con este historial,
+              el país no cuenta con un equipo de respuesta rápida o suministros para hacer frente a las consecuencias. Maatram tiene
+                la intención de cerrar esta brecha mediante la creación de un equipo de respuesta para áreas sensibles.
+            </p>
+            <h3>Planes de autoempleo y alivio de la pobreza.</h3>
+            <p>
+            La autosuficiencia es la verdadera independencia. Ayudamos a las personas a ser autosuficientes brindándoles habilidades
+            y ayudándolas a encontrar empleo o un medio para trabajar por cuenta propia.
+            </p>
+            <h3>Mejora social y económica para los desfavorecidos y oprimidos a través de la educación, la vivienda, la vivienda,
+              la organización y la facilitación de microayudas financieras a corto y largo plazo.
+            </h3>
+            <p>
+            Tenemos la intención de cerrar la brecha de la inequidad proporcionando educación, vivienda, vivienda y ayudas 
+            microfinancieras a corto y largo plazo a los pobres. Solo ayudándolos a volverse autosuficientes podemos lograr 
+            verdaderamente un maatram revolucionario social y económicamente.
+            </p>
+            <h3>Empoderamiento de la Mujer y Derechos Humanos.</h3>
+            <p>
+            Estamos en el siglo XXI y todavía no se ha avanzado en el campo del empoderamiento de la mujer y los derechos humanos.
+            Culpe a quien quiera, pero nosotros, que creemos en la reparación inmediata de los errores cometidos, ya estamos presionando
+              por el empoderamiento de las mujeres y la práctica del respeto de los derechos humanos básicos de uno mismo y de los demás.
+            </p>
+            <h3>Rehabilitación de Minusválidos, Indigentes, Viudas, Huérfanos, Geriatría, Desfavorecidos y Pacientes Crónicos (VIH/SIDA/Cáncer/T.B.etc.)</h3>
+            <p>To promote and organize self help groups for Men and Women.</p>
+            <h3>Abolición del trabajo infantil y Establecimiento de los derechos del niño.</h3>
+            <p>La pobreza ha engendrado una sociedad donde florece el trabajo infantil. Lo vemos en todas partes. Es un espectáculo tan común que pasa desapercibido,
+              incluso cuando nos mira a la cara. Es una cosa peligrosa, este punto al que hemos sido llevados, tan insensible que esta visión cotidiana de niños pequeños
+                cargando con la carga de un adulto, en un momento en que sus manos, pies y mentes deberían estar ocupados en diversión infantil, retozar. y el aprendizaje,
+                evoca poca o ninguna respuesta. Estamos tratando de generar más conciencia sobre un mal social que no debería necesitar tal esfuerzo pero que lamentablemente 
+                lo necesita. Estamos tratando de dar oportunidades de vida a estos niños atrapados en el círculo social de la pobreza, el abuso del alcohol y el analfabetismo.
+                  Estamos trabajando en el campo de los derechos del niño que es un paraguas más amplio y busca poner la infancia al alcance de los niños desfavorecidos.
+            </p>
+          </div>
         </div>
       </div>
       {/*Statistics*/}
