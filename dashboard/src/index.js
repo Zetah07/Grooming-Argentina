11f25daa0e2685971f71cad7df8ace488e89891b
akanashimi <andresvargas094@gmail.com>
2023-02-27 00:09:52 -0600

    DashBoard: Manage Blogs info

--- dashboard/src/index.js
+++ dashboard/src/index.js
@@ -6,17 +6,21 @@ import { AuthProvider } from "./contex/AuthProvider";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
+import { Provider } from "react-redux";
+import store from "./Redux/Store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-  <BrowserRouter>
-    <React.StrictMode>
-      <AuthProvider>
-        <Routes>
-          <Route path="*" element={<App />} />
-        </Routes>
-      </AuthProvider>
-    </React.StrictMode>
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <React.StrictMode>
+        <AuthProvider>
+          <Routes>
+            <Route path="*" element={<App />} />
+          </Routes>
+        </AuthProvider>
+      </React.StrictMode>
+    </BrowserRouter>
+  </Provider>
 );
