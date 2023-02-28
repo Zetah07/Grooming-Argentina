f35fed0b2cb1fdc5844394a902176649e5bafcc0
PatoProgramador <spartan1171947@hotmail.com>
2023-02-28 16:09:25 -0500

    Delete category functional

--- api/routes/category/categoryRouter.js
+++ api/routes/category/categoryRouter.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const { getCategories } = require("../../controllers/category/getCategories");
-const {handleCreateCategory} = require("../../controllers/category/createCategory");
+const { handleCreateCategory } = require("../../controllers/category/createCategory");
+const { deleteCategory } = require("../../controllers/category/deleteCategory");
 
 const router = express.Router();
 
 router.get("/", getCategories);
 router.post("/", handleCreateCategory);
+router.delete("/:id", deleteCategory );
 
 module.exports = router;
