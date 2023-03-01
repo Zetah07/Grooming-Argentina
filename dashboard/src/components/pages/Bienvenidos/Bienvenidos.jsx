51e012db2873257950735db857ae079e0a95c52d
JonathanG0m3z <104044651+JonathanG0m3z@users.noreply.github.com>
2023-03-01 16:43:56 -0500

    Students fixes

--- dashboard/src/components/pages/Bienvenidos/Bienvenidos.jsx
+++ dashboard/src/components/pages/Bienvenidos/Bienvenidos.jsx
@@ -5,7 +5,9 @@ const Bienvenidos = () => {
     return (
       <div className={style.container} >
         <h1 className={style.title}>Bienvenido</h1>
-        <p className={style.text}>Usuario: "user"</p>
+        <h2 className={style.h2}>Usuario: pato batman programador</h2>
+        <p className={style.p}>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum 
+          is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.</p>
       </div>
     )
   }
