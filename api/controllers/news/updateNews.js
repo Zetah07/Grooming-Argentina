5a455082a7ccfdd6c154217b2768b1588ee7e632
PatoProgramador <spartan1171947@hotmail.com>
2023-02-28 21:13:31 -0500

    Pagination for news, peding filters with pagination

--- api/controllers/news/updateNews.js
+++ api/controllers/news/updateNews.js
@@ -8,26 +8,30 @@ const modifyNews = async (req, res) => {
 
     if (!ObjectID.isValid(id)) return res.status(400).json({ message: "ID no válida" });
 
-    news.updateOne(
-        {_id: id},
-        // {$set: {title: title, description: description, img: img, category: category, link: link, provinceOrLocation: provinceOrLocation}},
-        {$set: body},
-        (err, docs) => {
-            if(err) {
-                if(err.path === "_id") {
-                    res.status(400).json({"error": "La noticia no existe"})
+    try {
+        news.updateOne(
+            {_id: id},
+            // {$set: {title: title, description: description, img: img, category: category, link: link, provinceOrLocation: provinceOrLocation}},
+            {$set: body},
+            (err, docs) => {
+                if(err) {
+                    if(err.path === "_id") {
+                        res.status(400).json({"error": "La noticia no existe"})
+                    } else {
+                        res.status(400).json({"error": err});
+                    }
                 } else {
-                    res.status(400).json({"error": err});
-                }
-            } else {
-                if(docs.matchedCount === 1) {
-                    res.status(200).json({"message": "Noticia actualizada"});
-                } else {
-                    res.status(400).json({"message": "No se puedo actualizar la noticia"});
+                    if(docs.matchedCount === 1) {
+                        res.status(200).json({"message": "Noticia actualizada"});
+                    } else {
+                        res.status(400).json({"message": "No se puedo actualizar la noticia"});
+                    }
                 }
             }
-        }
-    )
+        )
+    } catch (error) {
+        res.status(500).json({"message": error});
+    };
 };
 
 module.exports = { modifyNews };
