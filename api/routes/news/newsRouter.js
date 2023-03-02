b1665722e2b4cc2bcd467c20f1409e7a4f538eb8
PatoProgramador <spartan1171947@hotmail.com>
2023-03-02 17:12:38 -0500

    Delete news controller and route

--- api/routes/news/newsRouter.js
+++ api/routes/news/newsRouter.js
@@ -2,6 +2,7 @@ const express = require("express");
 const { getNews } = require("../../controllers/news/getNews");
 const { handleNews } = require("../../controllers/news/createNews");
 const { modifyNews } = require("../../controllers/news/updateNews");
+const { deleteNews } = require("../../controllers/news/deleteNews");
 const veryfyJWT = require("../../middleware/verifyJWT");
 const verifyRoles = require("../../middleware/verifyRoles");
 const upload = require('../../config/multer');
@@ -12,5 +13,6 @@ const router = express.Router();
 router.get("/", getNews);
 router.post("/", veryfyJWT, verifyRoles(["admin", "hr", "editor"]), upload.single("img"), handleNews);
 router.put("/:id", veryfyJWT, verifyRoles(["admin", "hr", "editor"]), modifyNews);
+router.delete("/:id", veryfyJWT, verifyRoles(["admin", "hr", "editor"]), deleteNews);
 
 module.exports = router
