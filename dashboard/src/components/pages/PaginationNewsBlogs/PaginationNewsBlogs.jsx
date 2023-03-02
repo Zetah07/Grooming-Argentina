595737d6996981576386f39deb2eb1efe8630d47
akanashimi <andresvargas094@gmail.com>
2023-03-02 16:45:41 -0600

    Pagination Dash Board, news and Blogs, useEffect with lag 999

---
+++ dashboard/src/components/pages/PaginationNewsBlogs/PaginationNewsBlogs.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import Pagination from 'react-bootstrap/Pagination';
+
+const PaginationNewsBlogs = ({ totalPages, firstHandler, prevHandler, nextHandler, lastHandler, pages, itemsPerPage, currentPage, pageNumberLimit, maxPageNumberLimit, minPageNumberLimit }) => {
+
+  const numOfPages = [];
+  const firstPage = 0;
+  let lastPage = 0;
+  const amountOfPages = totalPages;
+  for (let i = 0; i < amountOfPages; i++) {
+    lastPage = i;
+    numOfPages.push(i);
+  }
+
+  return (<Pagination>
+    <Pagination.First onClick={() => firstHandler(firstPage)} active={currentPage === firstPage} />
+    <Pagination.Prev onClick={prevHandler} active={currentPage === firstPage} />
+    <Pagination.Ellipsis />
+    {
+      numOfPages?.map((page) => {
+        if (page < maxPageNumberLimit && page >= minPageNumberLimit) {
+          return <Pagination.Item id={page} key={page} active={page === currentPage} onClick={() => pages(page)}>{page + 1}</Pagination.Item>
+        } else {
+          return null;
+        }
+      })
+    }
+    <Pagination.Ellipsis />
+    <Pagination.Next onClick={nextHandler} active={currentPage === lastPage} />
+    <Pagination.Last onClick={() => lastHandler(lastPage)} active={currentPage === lastPage} />
+  </Pagination >)
+}
+
+export default PaginationNewsBlogs
