99e0f2ada071708a45ea5c8d6967eeaa6edd957d
Zetah07 <sebastiancastro0792@gmail.com>
2023-03-03 19:30:52 -0500

    fix css in client// nav, landing, news, blog, contact.

--- client/src/components/Pages/Contact/Contact.jsx
+++ client/src/components/Pages/Contact/Contact.jsx
@@ -58,85 +58,93 @@ const Contact = () => {
 
   return (
     <section id="contact">
-      <h2 className={style.title}>Ponerse en contacto</h2>
-      <div className={style.contactContainer}>
-        <div className={style.contact_container}>
-          <article className={style.contact_option}>
-            <h3 className={style.contact_link}>Email:</h3>
-            <a
-              href="mailto:contacto@groomingarg.org"
-              target="_blank"
-              rel="noopener noreferrer"
-              className={style.icon}
-            >
-              <i className="bi bi-envelope-at"></i>
-            </a>
-          </article>
+      <div className={style.container}>
+        <h2 className={style.title}>Ponerse en contacto</h2>
+        <div className={style.contactContainer}>
+          <div className={style.contact_container}>
+            <article className={style.contact_option}>
+              <h3 className={style.contact_link}>Email:</h3>
+              <a
+                href="mailto:contacto@groomingarg.org"
+                target="_blank"
+                rel="noopener noreferrer"
+                className={style.icon}
+              >
+                <i className="bi bi-envelope-at"></i>
+              </a>
+            </article>
 
-          <article className={style.contact_option}>
-            <h3 className={style.contact_link}>WhatsApp:</h3>
-            <a
-              href="https://wa.me/+5491124811722"
-              target="_blank"
-              rel="noopener noreferrer"
-              className={style.icon}
-            >
-              <i className="bi bi-whatsapp"></i>
-            </a>
-          </article>
+            <article className={style.contact_option}>
+              <h3 className={style.contact_link}>WhatsApp:</h3>
+              <a
+                href="https://wa.me/+5491124811722"
+                target="_blank"
+                rel="noopener noreferrer"
+                className={style.icon}
+              >
+                <i className="bi bi-whatsapp " style={{ width: 32 }}></i>
+              </a>
+            </article>
+          </div>
         </div>
+
+        <Form ref={form} onSubmit={sendEmail} className={style.contactForm}>
+          <Form.Group controlId="formName">
+            <Form.Label className={style.form_label}>
+              Nombre completo:
+            </Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Escribe tu nombre completo"
+              required
+              className={style.formControl}
+              name="name"
+            />
+          </Form.Group>
+
+          <Form.Group controlId="formEmail">
+            <Form.Label className={style.form_label}>Email:</Form.Label>
+            <Form.Control
+              type="email"
+              placeholder="Escribe tu email"
+              required
+              className={style.formControl}
+              name="email"
+            />
+          </Form.Group>
+
+          <Form.Group controlId="formSubject">
+            <Form.Label className={style.form_label}>Asunto:</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Escribe el asunto"
+              required
+              className={style.formControl}
+              name="subject"
+            />
+          </Form.Group>
+
+          <Form.Group controlId="formMessage">
+            <Form.Label className={style.form_label}>Mensaje:</Form.Label>
+            <Form.Control
+              as="textarea"
+              rows={5}
+              placeholder="Escribe tu mensaje"
+              required
+              className={style.formControl}
+              name="message"
+            />
+          </Form.Group>
+
+          <Button
+            style={{ backgroundColor: "#004b82", borderColor: "#004b82" }}
+            type="submit"
+            className={style.submitButton}
+          >
+            Enviar
+          </Button>
+        </Form>
       </div>
-
-      <Form ref={form} onSubmit={sendEmail} className={style.contactForm}>
-        <Form.Group controlId="formName">
-          <Form.Label className={style.form_label}>Nombre completo:</Form.Label>
-          <Form.Control
-            type="text"
-            placeholder="Escribe tu nombre completo"
-            required
-            className={style.formControl}
-            name="name"
-          />
-        </Form.Group>
-
-        <Form.Group controlId="formEmail">
-          <Form.Label className={style.form_label}>Email:</Form.Label>
-          <Form.Control
-            type="email"
-            placeholder="Escribe tu email"
-            required
-            className={style.formControl}
-            name="email"
-          />
-        </Form.Group>
-
-        <Form.Group controlId="formSubject">
-          <Form.Label className={style.form_label}>Asunto:</Form.Label>
-          <Form.Control
-            type="text"
-            placeholder="Escribe el asunto"
-            required
-            className={style.formControl}
-            name="subject"
-          />
-        </Form.Group>
-
-        <Form.Group controlId="formMessage">
-          <Form.Label className={style.form_label}>Mensaje:</Form.Label>
-          <Form.Control
-            as="textarea"
-            rows={5}
-            placeholder="Escribe tu mensaje"
-            required
-            className={style.formControl}
-            name="message"
-          />
-        </Form.Group>
-
-        <Button variant="primary" type="submit" className={style.submitButton}>
-          Enviar
-        </Button>
-      </Form>
     </section>
   );
 };
