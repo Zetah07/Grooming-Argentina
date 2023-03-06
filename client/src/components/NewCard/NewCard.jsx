1d144581852b127b0f6cb9f42a1c953b9350f5b1
akanashimi <andresvargas094@gmail.com>
2023-03-06 13:38:34 -0600

    Pagination with search by name and title functional in client

--- client/src/components/NewCard/NewCard.jsx
+++ client/src/components/NewCard/NewCard.jsx
@@ -22,31 +22,35 @@ const NewCard = ({ id, image, title, createdAt, category, provinceOrLocation }) 
                 <img src={image} alt={title} className={`card-img-top ${style.img}`} />
                 <div className="card-body">
                     <div className={style.titleContainer}>
-                    <h5 className={`card-title ${style.title}`}>{title}</h5>
+                        <h5 className={`card-title ${style.title}`}>{title}</h5>
                     </div>
                     <div className={style.information}>
-                    <p className="card-subtitle mb-2 text-muted">Categoria: {category}</p>
-                    <p className="card-subtitle mb-2 text-muted">Provincia: {provinceOrLocation}</p>
-                    <p className="card-subtitle mb-2 text-muted">Publicado: {createdAt}</p>
+                        <p className="card-subtitle mb-2 text-muted">Categorias:
+                            {category.map(cat => {
+                                return <span className="card-subtitle mb-2 text-muted">|{cat}| </span>
+                            })}
+                        </p>
+                        <p className="card-subtitle mb-2 text-muted">Provincia: {provinceOrLocation}</p>
+                        <p className="card-subtitle mb-2 text-muted">Publicado: {createdAt}</p>
                     </div>
                     <div className="card-body">
                         <div className={style.moreButton}>
-                        <FacebookShareButton url={`http://localhost:3000/noticias/${id}`} quote={title}>
-                            <FacebookIcon size={40} round={true} />
-                        </FacebookShareButton>
-                        <LinkedinShareButton url={`http://localhost:3000/noticias/${id}`} title={title}>
-                            <LinkedinIcon size={40} round={true} />
-                        </LinkedinShareButton>
-                        <TelegramShareButton url={`http://localhost:3000/noticias/${id}`} title={title}>
-                            <TelegramIcon size={40} round={true} />
-                        </TelegramShareButton>
-                        <TwitterShareButton url={`http://localhost:3000/noticias/${id}`} title={title}>
-                            <TwitterIcon size={40} round={true} />
-                        </TwitterShareButton>
-                        <WhatsappShareButton url={`http://localhost:3000/noticias/${id}`} title={title}>
-                            <WhatsappIcon size={40} round={true} />
-                        </WhatsappShareButton>
-                        <Button href={`/noticias/${id}`} variant="primary">Leer más</Button>
+                            <FacebookShareButton url={`http://localhost:3000/noticias/${id}`} quote={title}>
+                                <FacebookIcon size={40} round={true} />
+                            </FacebookShareButton>
+                            <LinkedinShareButton url={`http://localhost:3000/noticias/${id}`} title={title}>
+                                <LinkedinIcon size={40} round={true} />
+                            </LinkedinShareButton>
+                            <TelegramShareButton url={`http://localhost:3000/noticias/${id}`} title={title}>
+                                <TelegramIcon size={40} round={true} />
+                            </TelegramShareButton>
+                            <TwitterShareButton url={`http://localhost:3000/noticias/${id}`} title={title}>
+                                <TwitterIcon size={40} round={true} />
+                            </TwitterShareButton>
+                            <WhatsappShareButton url={`http://localhost:3000/noticias/${id}`} title={title}>
+                                <WhatsappIcon size={40} round={true} />
+                            </WhatsappShareButton>
+                            <Button href={`/noticias/${id}`} variant="primary">Leer más</Button>
                         </div>
                     </div>
                 </div>
