e55dadec2e814c5e1965fdc60c4ecce8d2f929f3
multichiki22 <myguelangel12@gmail.com>
2023-03-07 01:51:33 -0500

    Bunch of fixes (deleting user, welcome page,newUser etc)

--- dashboard/src/components/pages/Bienvenidos/Bienvenidos.jsx
+++ dashboard/src/components/pages/Bienvenidos/Bienvenidos.jsx
@@ -1,30 +1,36 @@
-// import React, { useEffect } from "react";
-// import useAuth from "../../../hooks/useAuth.js";
-import style from "./Bienvenidos.module.css";
-// import axios from "axios";
+import React, { useEffect } from "react";
+import useAuth from "../../../hooks/useAuth.js";
+import { useState } from "react";
+import axios from "../../../api/axios";
+import logo from "../../../assets/Grooming_Logo.png";
 
 const Bienvenidos = () => {
-  // const { user, auth } = useAuth();
+  const { auth } = useAuth();
+  const [name, setName] = useState("Usuario");
 
-  // useEffect(() => {
-  //   try{
-  //     axios.get("http://localhost:4000/panel/bienvenidos", {
-  //       withCredentials: true,
-  //       headers: {
-  //         Authorization: `Bearer ${auth?.accessToken}`,
-  //       },
-  //     });
-  //   } catch (error) {
-  //     console.error(error);
-  //   }
-  // }, [auth]);
+  const getUserName = async () => {
+    try {
+      const user = await axios.get(`/users/${auth.user}`, {
+        withCredentials: true,
+        headers: {
+          Authorization: `Bearer ${auth?.accessToken}`,
+        },
+      });
+      setName(user.data.name);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    getUserName();
+  }, []);
 
   return (
-    <div className={style.container}>
-        <div>
-          <h1>Bienvenido patoamigos</h1>
-          <h2>Estas en la pagina de Bienvenidos</h2>
-        </div>
+    <div className="container" style={{display:"flex", justifyContent:"center",height:"90vh",alignItems:"center",flexDirection:"column"}}>
+        <img src={logo} alt="Logo grooming Argentina" style={{width:"20rem"}}/>
+        <h1 style={{fontFamily:"Titulo Gotham", fontSize:"3rem",color:"#004b82"}}>Bienvenido:</h1>
+        <h1 style={{fontFamily:"Titulo Gotham", fontSize:"3rem",color:"#004b82"}}>{name}</h1>
     </div>
   );
 };
