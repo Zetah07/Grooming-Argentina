23ce32dcd9ca8d1ff7057ee5be886e81111cd3c1
multichiki22 <myguelangel12@gmail.com>
2023-03-07 23:40:39 -0500

    testing

--- api/config/corsOptions.js
+++ api/config/corsOptions.js
@@ -1,10 +1,12 @@
 const  allowOrigins = require("./allowOrigins")
-
+console.log("cors ,cors options");
 const corsOptions = {
     origin: (origin, callback) =>{
         if (!origin || allowOrigins.indexOf(origin)!== -1){
+            console.log("true ,cors options");
             callback(null, true)
         }else {
+            console.log("false ,cors options");
             callback(new Error("test"))
         }     
     },
