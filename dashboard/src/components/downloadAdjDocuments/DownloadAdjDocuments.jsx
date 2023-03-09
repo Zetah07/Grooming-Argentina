9857c91f120dbfd92d0010209d6ef21e9535218b
multichiki22 <myguelangel12@gmail.com>
2023-03-08 19:08:37 -0500

    Added firebase upload of pdfs

--- dashboard/src/components/downloadAdjDocuments/DownloadAdjDocuments.jsx
+++ dashboard/src/components/downloadAdjDocuments/DownloadAdjDocuments.jsx
@@ -2,31 +2,10 @@ import React from "react";
 import axios from "axios";
 import Button from "react-bootstrap/Button";
 
-const DownloadPDFButton = ({ userId, documentType }) => {
-  const handleDownload = async () => {
-    try {
-      const response = await axios.post(
-        "/documents",
-        { [documentType]: userId },
-        {
-          responseType: "blob",
-        }
-      );
 
-      const url = window.URL.createObjectURL(new Blob([response.data]));
-      const link = document.createElement("a");
-      link.href = url;
-      link.setAttribute("download", `${userId}.pdf`);
-      document.body.appendChild(link);
-      link.click();
-      link.remove();
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
+const DownloadPDFButton = ({ documentType }) => {
   return (
-    <Button onClick={handleDownload}>
+    <Button>
       Descargar documento adjunto {documentType === "CV" ? "CV" : "AdjDocument"}
     </Button>
   );
