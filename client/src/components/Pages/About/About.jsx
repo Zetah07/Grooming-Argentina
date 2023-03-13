9514b32bd256a581ebc8269c6027380b3928c8e2
Zetah07 <sebastiancastro0792@gmail.com>
2023-03-13 11:22:02 -0500

    changes in blog module

--- client/src/components/Pages/About/About.jsx
+++ client/src/components/Pages/About/About.jsx
@@ -5,10 +5,6 @@ import folder from "../../../assets/About/BsFolderCheck.png";
 import volunteer from "../../../assets/About/MdPeopleAlt.png";
 import followers from "../../../assets/About/IoShareSocialOutline.png";
 import provincies from "../../../assets/About/FiMapPin.png";
-import folder from "../../../assets/About/BsFolderCheck.png";
-import volunteer from "../../../assets/About/MdPeopleAlt.png";
-import followers from "../../../assets/About/IoShareSocialOutline.png";
-import provincies from "../../../assets/About/FiMapPin.png";
 import s from "./About.module.css";
 // import banner from '../../../assets/18.png'
 // import team from "../About/AboutPractice";
